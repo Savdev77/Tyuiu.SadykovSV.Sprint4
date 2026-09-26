1850c9948d6f7ce7591b99c407ab8a076922104d
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-26 18:33:05 +0500

    Task3

---
+++ Tyuiu.SadykovSV.Sprint4.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SadykovSV.Sprint4.Task3.V5.Lib
+{
+    public class DataService : ISprint4Task3V5
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+
+            int res = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                res += array[i, 0];
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SadykovSV.Sprint4.Task3.V5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.SadykovSV.Sprint4.Task3.V5.Lib;
+
+Console.Title = "Спринт #4 | Выполнил: Садыков С.В. | ПИНб-26-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #5                                                              *");
+Console.WriteLine("* Выполнил: Садыков С.В. | ПИНб-26-1                                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* статическими значениями в диапазоне от 2 до 9. Найдите сумму            *");
+Console.WriteLine("* элементов в первом столбце массива.                                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+
+int[,] array = new int[5, 5] { { 3, 4, 8, 6, 3 },
+                               { 9, 9, 3, 6, 9 },
+                               { 5, 6, 7, 2, 3 },
+                               { 3, 6, 2, 3, 8 },
+                               { 5, 7, 2, 6, 4 } };
+
+int rows = array.GetUpperBound(0) + 1;
+int columns = array.GetUpperBound(1) + 1;
+
+Console.WriteLine("Массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(array);
+Console.WriteLine($"Сумма элементов в первом столбце: {res}");
+
+Console.ReadKey();
